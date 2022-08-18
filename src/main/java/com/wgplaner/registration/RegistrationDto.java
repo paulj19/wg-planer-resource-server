aee9750802c78f503cf6bcaf2028ed3cf12f92fd
paul <s8dipaul@stud.uni-saarland.de>
2022-08-19 01:47:43 +0200

    wip test

--- src/main/java/com/wgplaner/registration/RegistrationDto.java
+++ src/main/java/com/wgplaner/registration/RegistrationDto.java
@@ -2,26 +2,15 @@ package com.wgplaner.registration;
 
 import com.wgplaner.common.validation.email.ValidEmail;
 import com.wgplaner.entity.User;
-import lombok.Data;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 
-@Data
-public class RegistrationDto {
-
-    @NotBlank
-    private String username;
-    @NotNull
-    @NotBlank
-    @ValidEmail
-    private String email;
-    @NotBlank
-    private String password;
-    @NotBlank
-    private String confirmPassword;
+public record RegistrationDto(@NotBlank String username,
+                              @NotNull @NotBlank @ValidEmail String email,
+                              @NotBlank String password, @NotBlank String confirmPassword) {
 
     public User mapToUser(PasswordEncoder passwordEncoder) {
         return new User(username, passwordEncoder.encode(password), email);
--- src/test/java/com/wgplaner/registration/RegistrationControllerTest.java
+++ src/test/java/com/wgplaner/registration/RegistrationControllerTest.java
@@ -1,5 +1,58 @@
 package com.wgplaner.registration;
 
-class RegistrationControllerTest {
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RegistrationControllerTest {
+    @Autowired
+    private RegistrationController registrationController;
+
+    private MockMvc mockMvc;
+    private RegistrationDto registrationDto;
+
+    @BeforeEach
+    public void setup() throws JsonProcessingException {
+        mockMvc = MockMvcBuilders.standaloneSetup(registrationController).build();
+        registrationDto = getRegistrationDto();
+    }
+
+    @Test
+    public void whenPostWithValidData_shouldCreateAndRespondWithNewCreatedUser() throws Exception {
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/register" ).contentType(MediaType.APPLICATION_JSON).content(asJsonString(registrationDto)));
+
+        //assert
+        resultActions.andExpect(MockMvcResultMatchers.status().isCreated());
+        resultActions.andExpect(content().string(asJsonString(registrationDto)));
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private RegistrationDto getRegistrationDto() {
+        return new RegistrationDto("username", "foo@email.com", "Password123!", "Password123!");
+    }
 
 }
